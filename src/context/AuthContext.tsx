3dc5851984573c2991e73e9528c0333ca823c63f
Gabriel <gabriel.ifsal@gmail.com>
2022-08-26 13:58:20 -0300

    Adding user data on AuthContext and creating sessions on form submit

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,4 +1,27 @@
-import { createContext, ReactNode, useCallback, useContext } from 'react';
+import {
+  createContext,
+  ReactNode,
+  useCallback,
+  useContext,
+  useState,
+} from 'react';
+import { api } from '~/services/api';
+
+import Router from 'next/router';
+
+interface User {
+  email: string;
+  permissions: string[];
+  roles: string[];
+}
+
+interface Jwt {
+  exp: number;
+  iat: number;
+  sub: string;
+  permissions: string[];
+  roles: string[];
+}
 
 interface SignInCredentials {
   email: string;
@@ -8,6 +31,7 @@ interface SignInCredentials {
 interface AuthContextData {
   isAuthenticated: boolean;
   signIn(credentials: SignInCredentials): Promise<void>;
+  user: User;
 }
 
 interface AuthProviderProps {
@@ -17,14 +41,25 @@ interface AuthProviderProps {
 const AuthContext = createContext({} as AuthContextData);
 
 function AuthProvider({ children }: AuthProviderProps) {
-  const isAuthenticated = false;
+  const [user, setUser] = useState<User>();
+  const isAuthenticated = !!user;
 
   const signIn = useCallback(async ({ email, password }: SignInCredentials) => {
-    console.log({ email, password });
+    try {
+      const response = await api.post('/sessions', { email, password });
+      const { token, permissions, roles } = response.data;
+
+      setUser({ email, permissions, roles });
+
+      Router.push('/dashboard');
+      console.log(user);
+    } catch (err) {
+      console.log(err);
+    }
   }, []);
 
   return (
-    <AuthContext.Provider value={{ isAuthenticated, signIn }}>
+    <AuthContext.Provider value={{ isAuthenticated, signIn, user }}>
       {children}
     </AuthContext.Provider>
   );
